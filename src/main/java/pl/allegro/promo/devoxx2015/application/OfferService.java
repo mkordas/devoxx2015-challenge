8f941d89be23dc3a9df7efc5c61706e15e47454a
Michal Kordas <kontakt@michalkordas.com>
2015-06-24 01:58:29 +0200

    Add missing implementation to OfferService

--- src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
+++ src/main/java/pl/allegro/promo/devoxx2015/application/OfferService.java
@@ -2,6 +2,7 @@ package pl.allegro.promo.devoxx2015.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import pl.allegro.promo.devoxx2015.domain.Offer;
 import pl.allegro.promo.devoxx2015.domain.OfferRepository;
 import pl.allegro.promo.devoxx2015.domain.PhotoScoreSource;
@@ -21,7 +22,18 @@ public class OfferService {
     }
 
     public void processOffers(List<OfferPublishedEvent> events) {
-        // TODO save offers with pretty photos
+        events.stream().map(e -> new Offer(e.getId(), e.getTitle(), e.getPhotoUrl(), getScore(e)))
+                .filter(Offer::hasPrettyPhoto)
+                .sorted((o1, o2) -> Double.compare(o2.getPhotoScore(), o1.getPhotoScore()))
+                .forEach(offerRepository::save);
+    }
+
+    private double getScore(OfferPublishedEvent offerPublishedEvent) {
+        try {
+            return photoScoreSource.getScore(offerPublishedEvent.getPhotoUrl());
+        } catch (RestClientException ignored) {
+            return 0.7;
+        }
     }
 
     public List<Offer> getOffers() {
